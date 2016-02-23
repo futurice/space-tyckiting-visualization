145ec6478c23019e684c12a7cf740bf145a81c84
Timo Moisio <timohmoisio@gmail.com>
2016-02-23 10:16:39 +0200

    Add notifying embedding page when game is ready

--- space-tyckiting/Assets/Scripts/Behaviours/GameDataLoader.cs
+++ space-tyckiting/Assets/Scripts/Behaviours/GameDataLoader.cs
@@ -33,36 +33,45 @@ namespace SpaceTyckiting
 
 		void Start()
 		{
-			string[] arguments = System.Environment.GetCommandLineArgs();
+			IsLoading = false;
 
-			foreach (var arg in arguments)
+			if (Application.isEditor) 
 			{
-				var parts = arg.Split(new string[]{"="}, System.StringSplitOptions.RemoveEmptyEntries);
-				Debug.Log(string.Join(" ", parts));
-				if (parts.Length == 2 && parts[0] == "timescale")
+				LoadGame (null);
+			}
+			else if (Application.isWebPlayer || Application.platform == RuntimePlatform.WebGLPlayer) 
+			{
+				Application.ExternalCall ("OnUnityReady");
+			}
+			else 
+			{
+				string[] arguments = System.Environment.GetCommandLineArgs();
+
+				foreach (var arg in arguments)
 				{
-					int timeScale = 1;
-					System.Int32.TryParse(parts[1], out timeScale);
-					Time.timeScale = timeScale;
-				}
-				else if (parts.Length == 2 && parts[0] == "path")
-				{
-#if UNITY_STANDALONE_WIN
-					LoadGame("file:///" + parts[1]);
-#endif
-#if UNITY_STANDALONE_OSX
-					LoadGame("file://" + parts[1]);
-#endif
-				}
-				else if (parts.Length == 2 && parts[0] == "url")
-				{
-					StartCoroutine(LoadGame_Coroutine(parts[1]));
+					var parts = arg.Split(new string[]{"="}, System.StringSplitOptions.RemoveEmptyEntries);
+					Debug.Log(string.Join(" ", parts));
+					if (parts.Length == 2 && parts[0] == "timescale")
+					{
+						int timeScale = 1;
+						System.Int32.TryParse(parts[1], out timeScale);
+						Time.timeScale = timeScale;
+					}
+					else if (parts.Length == 2 && parts[0] == "path")
+					{
+						#if UNITY_STANDALONE_WIN
+						LoadGame("file:///" + parts[1]);
+						#endif
+						#if UNITY_STANDALONE_OSX
+						LoadGame("file://" + parts[1]);
+						#endif
+					}
+					else if (parts.Length == 2 && parts[0] == "url")
+					{
+						StartCoroutine(LoadGame_Coroutine(parts[1]));
+					}
 				}
 			}
-
-			if (Application.isEditor) LoadGame(null);
-
-			IsLoading = false;
 		}
 
 		public void LoadGame(string url)
